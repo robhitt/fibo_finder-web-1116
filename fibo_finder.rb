7e54b6c88afc8046ca0f92599a06d0c17053b0ad
Rob Hitt <robhitt@gmail.com>
2017-02-22 22:32:19 -0500

    done

--- fibo_finder.rb
+++ fibo_finder.rb
@@ -1,3 +1,19 @@
 def fibo_finder(n)
-  # code goes here
+  if n == 0
+    n
+  elsif n == 1
+    n
+  elsif n == 2
+    1
+  else
+    n = fibo_finder(n-1) + fibo_finder(n-2)
+  end
 end
+
+# puts fibo_finder(0)
+# puts fibo_finder(1)
+# puts fibo_finder(2)
+# puts fibo_finder(3)
+# puts fibo_finder(4)
+# puts fibo_finder(7)
+# puts fibo_finder(10)
